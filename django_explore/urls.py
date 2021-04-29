af207e5d2eb24fcec0975f6f25c2d232f009a0f9
lilong01 <lilong01@xiaoduotech.com>
2021-04-29 14:52:04 +0800

    111

--- django_explore/urls.py
+++ django_explore/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from foo.views import sayHello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'sayhello/', sayHello)
 ]
--- foo/views.py
+++ foo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import datetime
+from django.http import HttpResponse
+
+
+def sayHello(request):
+    s = '技术的高房价但是广泛接受的但是看见浩方空间的!'
+    current_time = datetime.datetime.now()
+    html = '<html><head></head><body><h1> %s </h1><p> %s </p></body></html>' % (s, current_time)
+    return HttpResponse(html)
+
 
 # Create your views here.
