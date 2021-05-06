4406ca9c1e434ce63de974767620595b067d23a9
lilong01 <lilong01@xiaoduotech.com>
2021-05-06 15:44:41 +0800

    8950

--- django_explore/settings.py
+++ django_explore/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hans'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Shanghai'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
--- foo/admin.py
+++ foo/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.contrib import admin
 from foo.models import BookInfo
+from foo.models import HeroInfo
+from foo.models import User
 
 admin.site.register(BookInfo)
+admin.site.register(HeroInfo)
+admin.site.register(User)
 
 # Register your models here.
