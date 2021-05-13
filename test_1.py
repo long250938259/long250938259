f23c937da2cf6d8a85db074a391e0ab3b4f8415f
lilong01 <lilong01@xiaoduotech.com>
2021-05-13 10:27:59 +0800

    35343333

--- test_1.py
+++ test_1.py
@@ -1,14 +1,21 @@
-flag0 = 1 == 1
-flag1 = 3 > 2
-flag2 = 2 < 1
-flag3 = flag1 and flag2
-flag4 = flag1 or flag2
-flag5 = not (1 != 2)
-print('flag0 =', flag0) # flag0 = True
-print('flag1 =', flag1) # flag1 = True
-print('flag2 =', flag2) # flag2 = False
-print('flag3 =', flag3) # flag3 = False
-print('flag4 =', flag4) # flag4 = True
-print('flag5 =', flag5) # flag5 = False
-print(flag1 is True) # True
-print(flag2 is not False) # False
+from multiprocessing import Process
+from time import sleep
+
+counter = 0
+
+
+def sub_task(string):
+    global counter
+    while counter < 10:
+        print(string, end='', flush=True)
+        counter += 1
+        sleep(1)
+
+
+def main():
+    Process(target=sub_task, args=('Ping',)).start()
+    Process(target=sub_task, args=('Pong',)).start()
+
+
+if __name__ == '__main__':
+    main()
