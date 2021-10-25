cdee2e1802127640951b8ce2c994c20dc7a9f3c0
lilong <lilong@xiaoduotech.com>
2021-10-25 19:32:42 +0800

    修改main文件

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import pytest
 
 
 if __name__ == "__main__":
-    pytest.main(["-s", "pytest_test/options/test_in_theaters.py", "--alluredir", 'report'])
+    pytest.main(["-s", "pytest_test/options/test_file.py", "--alluredir", 'report'])
